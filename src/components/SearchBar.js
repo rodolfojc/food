12abf8c41722f5cb44c07a6705c8148654d6cac3
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-13 18:41:53 +0000

    Displaying Icons

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
     return(
         <View style={styles.background}>
+            <Feather name="search" size={30}/>
             <Text>Search Bar</Text>
         </View>
     );
@@ -15,7 +17,7 @@ const styles = StyleSheet.create({
         height: 50,
         borderRadius: 5,
         marginHorizontal: 15
-    }
+    },    
 });
 
 export default SearchBar;
