0c122f617f1b0c3a381b5eac230b22fe70d11fbd
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-13 18:28:53 +0000

    Search Bar Component - Init

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchBar = () => {
+    return(
+        <View>
+            <Text>Welcome to Search Page</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchBar;
