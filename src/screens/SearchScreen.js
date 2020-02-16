2f8e39035633a6b0eb8d04a72821a5729dd917b9
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-16 18:46:11 +0000

    Empty Elements

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -18,7 +18,7 @@ const SearchScreen = () => {
     };
 
     return(
-        <View style={{ flex: 1}}>
+        <>
             <SearchBar 
                 term={term}
                 onTermChange={setTerm}
@@ -40,7 +40,7 @@ const SearchScreen = () => {
                     results={filterResultsByPrice('$$$')}
                 />
             </ScrollView>
-        </View>
+        </>
     );
 };
 
