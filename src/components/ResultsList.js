6ef4ebf0c98f9456bec78dcd290c79af53b3c48d
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-16 19:23:48 +0000

    Last Fix

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -11,6 +11,11 @@ import {withNavigation} from 'react-navigation';
 import ResultsDetails from './ResultsDetails';
 
 const ResultsList = ({ title, results, navigation }) => {
+    
+    if (!results.length) {
+        return null;
+    }
+
     return(
         <View styles={styles.container}>
             <Text style={styles.title}>{title}</Text>
